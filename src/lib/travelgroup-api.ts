d924b899b848bc08db60d2db93a67c4cd5e0b4f0
seoseuo <seoseuo@naver.com>
2025-03-04 18:20:29 +0900

    feat : 모임 생성 기능 구현

--- src/lib/travelgroup-api.ts
+++ src/lib/travelgroup-api.ts
@@ -1,15 +1,12 @@
 import axios, { AxiosError } from "axios";
 import Cookies from "js-cookie"; // js-cookie 라이브러리 사용
-import { useRouter } from "next/navigation";
+
 
 // 환경 변수에서 API 주소 가져오기
 const API_BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL;
 
 const api = axios.create({
   baseURL: API_BASE_URL,
-  headers: {
-    "Content-Type": "application/json",
-  },
   withCredentials: true, // 쿠키를 포함하려면 true로 설정
 });
 
@@ -17,7 +14,7 @@ const api = axios.create({
 const getAuthTokens = () => {
   // const accessToken = Cookies.get("accessToken");
   const accessToken =
-    "eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ3YW5uYWdvIiwiZXhwIjoxNzQxMDAxMDY4LCJVU05BTUUiOiLquYDsnbjsp4EiLCJVU0VNQUlMIjoiZ2Ftc3RAbmF2ZXIuY29tIiwiVVNJRFgiOjIsIlVTUFJPRklMRSI6InByb2ZpbGUucG5nIiwiVVNTVEFURSI6MX0.r0LQiSg4JM5o--Ryk6TuxIJZz_vmblzI-25sACAGvps";
+    "eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ3YW5uYWdvIiwiZXhwIjoxNzQxMDgzMTM2LCJVU05BTUUiOiLquYDsnbjsp4EiLCJVU0VNQUlMIjoiZ2Ftc3RAbmF2ZXIuY29tIiwiVVNJRFgiOjIsIlVTUFJPRklMRSI6InByb2ZpbGUucG5nIiwiVVNTVEFURSI6MX0.bwDS9T2Q53o-g_1kaIcfddOmEpCtlxBX1kal_otupIs";
   const refreshToken = Cookies.get("refreshToken");
   return { accessToken, refreshToken };
 };
@@ -50,13 +47,28 @@ export async function getTravelGroupList() {
   }
 }
 
+export const postGroup = async (formData: FormData) => {
+  try {
+    console.log("postGroup 호출");
+    console.log(formData);
+    const response = await api.post('/travelgroups', formData, {
+      headers: {
+        'Content-Type': 'multipart/form-data',
+      },
+    });
+    console.log(response.data); 
+    location.href = '/travelgroups';
+  } catch (error) {
+    console.error('Error posting group:', error);
+    throw error;
+  }
+};
 
 export async function getTravelGroup(grIdx: string) {
   // 여기에 로직 추가 (예: API 호출 등)
   try {
     const response = await api.get("/travelgroups/"+grIdx); 
-    console.log(response.data);
-    
+    console.log(response.data);    
     return response.data;
   } catch (error: unknown) {
     if (error instanceof AxiosError && error.response) {      

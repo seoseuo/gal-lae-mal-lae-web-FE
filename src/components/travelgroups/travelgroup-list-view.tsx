999424d45dd2fba4fe7125316b0df3dcd8d8da03
seoseuo <seoseuo@naver.com>
2025-03-06 19:06:27 +0900

    feat : 여행지 생성 사이클 구현 완료

--- src/components/travelgroups/travelgroup-list-view.tsx
+++ src/components/travelgroups/travelgroup-list-view.tsx
@@ -10,23 +10,29 @@ export default function TravelgroupListView({ travelGroupList }: { travelGroupLi
                 travelGroupList.map((group) => (
                     <div key={group.grIdx} onClick={() => {
                         router.push(`/travelgroups/get`);
-                        // 로컬 스토리지에 지금 grIdx 저장
                         localStorage.setItem('grIdx', group.grIdx.toString());
                     }}>
-                        <div className="travelgroup-list-view-container" style={{ cursor: 'pointer' }}>
-                            <img className="travelgroup-list-view-img" src={`${NEXT_PUBLIC_UPLOAD_PATH}${group.grProfile}`} alt={group.grName} />
-                            <div className="travelgroup-list-view-tag-container">
+                        <div className="travelgroup-list-view-container" style={{ cursor: 'pointer', minWidth: '400px' }}>
+                            <div style={{ flex: '0 0 121px' }}>
+                                <img className="travelgroup-list-view-img" src={`${NEXT_PUBLIC_UPLOAD_PATH}${group.grProfile}`} alt={group.grName} />
+                            </div>
+                            <div className="travelgroup-list-view-tag-container" style={{ flex: 1, minWidth: 0 }}>
                                 <div className="bold" style={{ fontSize: "20px", color: "#490085" }}>{group.grName}</div>
                                 <div style={{ marginTop: '20px' }}></div>
                                 <div className="travelgroup-list-view-tag">{group.grCount}명</div>
                                 {group.grLdList && group.grLdList.length > 0 && (
-                                    <div className="travelgroup-list-view-tag-row">
+                                    <div className="travelgroup-list-view-tag-row" style={{
+                                        overflowX: 'auto',
+                                        whiteSpace: 'nowrap',
+                                        width: '100%',
+                                        display: 'flex',
+                                        gap: '5px'
+                                    }}>
                                         {group.grLdList.map((item: string, index: number) => (
-                                            <div className="travelgroup-list-view-tag" key={index}>{item}</div>
+                                            <div className="travelgroup-list-view-tag" key={index} style={{flexShrink: 0}}>{item}</div>
                                         ))}
                                     </div>
                                 )}
-
                             </div>
                         </div>
                     </div>

35b3ffe69d42939d6da296d491eceec3865aba9a
seoseuo <seoseuo@naver.com>
2025-03-14 00:00:32 +0900

    feat : 랜덤 화면 기능 구현

--- src/app/travelgroups/travel/random-rendering/page.tsx
+++ src/app/travelgroups/travel/random-rendering/page.tsx
@@ -1,32 +1,45 @@
+"use client";
+
 import "@/styles/travelgroups/travelgroups-style.css";
+import { useEffect } from 'react';
+import { useRouter } from 'next/navigation';
 
+const Home = () => {
+    const router = useRouter();
 
-export default function Home() {
+    useEffect(() => {
+        const timer = setTimeout(() => {
+            router.push('/travelgroups/travel/random');
+        }, 3000);
+
+        const aElement = document.getElementById('a');
+        if (aElement) {
+            aElement.style.animation = 'rotate 4s linear infinite';
+        }
+
+        const dots = document.querySelectorAll<HTMLElement>('.random-dot');
+        dots.forEach((dot, index) => {
+            dot.style.animation = `dot-blink 1s ${index * 0.5}s linear infinite`;
+        });
+
+        return () => clearTimeout(timer);
+    }, [router]);
+
     return (
-        <div>
-
-            <br />
-            <br />
-            <br />
+        <div style={{ marginTop: '100px' }}>
             <div className="travelgroup-container">
-                <img src="/travelgroups/random.svg" alt="random" />
-
+                <img id="a" src="/travelgroups/random.svg" alt="random" />
                 <div className="random-dot-container">
-                    <div className="random-dot">
-                    </div>
-                    <div className="random-dot">
-                    </div>
-                    <div className="random-dot">
-                    </div>
+                    <div className="random-dot"></div>
+                    <div className="random-dot"></div>
+                    <div className="random-dot"></div>
                 </div>
-                <br />
-                <br />
-                <span className='regular' style={{ fontSize: '14px', textAlign: 'center' }}>
+                <span className='regular' style={{ fontSize: '14px', textAlign: 'center', marginTop: '40px' }}>
                     잠시만요 ! 모임에 딱 맞는 여행지를 찾고 있어요
                 </span>
-
-
             </div>
         </div>
     );
-}
+};
+
+export default Home;
--- src/app/travelgroups/travel/random/page.tsx
+++ src/app/travelgroups/travel/random/page.tsx
@@ -1,17 +1,45 @@
+"use client";
+
+import React, { useState, useEffect } from 'react';
 import Header from "../../../header";
 import "@/styles/travelgroups/travelgroups-style.css";
-
+import { getRandomTravel } from "@/lib/travelgroup-api";
 
 export default function Home() {
+
+    const [randomTravel, setRandomTravel] = useState(null);
+
+    useEffect(() => {
+        async function fetchRandomTravel() {
+            const travelData = await getRandomTravel();
+            setRandomTravel(travelData);
+        }
+        fetchRandomTravel();
+    }, []);
+
     return (
         <div>
             <Header text="여행지 설정" icon="back"></Header>
-            <span className='regular' style={{ fontSize: '14px', marginLeft: '10px' }}>ㅇㅇ도-ㅇㅇ시</span>
+            {randomTravel && (
+                <span className='regular' style={{ fontSize: '14px', marginLeft: '10px' }}>
+                    {randomTravel.ldIdx >= 1 && randomTravel.ldIdx <= 8 ? (
+                        randomTravel.ldName
+                    ) : (
+                        <>
+                            {randomTravel.locationDo.ldName} {randomTravel.lsName}
+                        </>
+                    )}
+                </span>
+            )}
+
             <br />
             <br />
             <div className="travelgroup-container">
-                <img src="/travelgroups/chuncheon.svg" alt="chuncheon" />
-
+                {randomTravel.ldIdx >= 1 && randomTravel.ldIdx <= 8 ? (
+                    <img className="travel-box-parent-container-img" src={`/travelgroups/location/${randomTravel.ldIdx}.svg`} alt={`/travelgroups/location/${randomTravel.ldIdx}.svg`} />
+                ) : (
+                    <img className="travel-box-parent-container-img" src={`/travelgroups/location/${randomTravel.ldIdx}-${randomTravel.lsIdx}.svg`} alt={`/travelgroups/location/${randomTravel.ldIdx}-${randomTravel.lsIdx}.svg`} />
+                )}
             </div>
 
             <br />
@@ -26,8 +54,6 @@ export default function Home() {
                 <button className="long-nomal-button bottom-button-postion travelgroups-font-size" style={{ top: '690px' }}>다시 랜덤</button>
                 <button className="long-active-button bottom-button-postion travelgroups-font-size">갈래요</button>
             </div>
-
-
 
         </div >
     );

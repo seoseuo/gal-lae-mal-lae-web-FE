4485cabe017126db21d67751529065dab3a9de52
seoseuo <seoseuo@naver.com>
2025-03-14 21:43:38 +0900

    feat : header 컴포넌트 변수 추가

--- src/app/header.tsx
+++ src/app/header.tsx
@@ -3,13 +3,13 @@
 import { useRouter } from 'next/navigation';
 
 // Header 컴포넌트
-export default function Header({ text, icon }: { text: string; icon: string }) {
+export default function Header({ text, icon, parent }: { text: string; icon: string, parent: string }) {
   const router = useRouter();
 
   return (
     <div>
       <p className="header">
-        <img src={`/${icon}.svg`} alt={`${icon}-icon`} className="header-icon" onClick={() => router.back()} style={{cursor:'pointer'}}/>
+        <img src={`/${icon}.svg`} alt={`${icon}-icon`} className="header-icon" onClick={() => router.push(`${parent}`)} style={{cursor:'pointer'}}/>
         <span className="header-text bold">{text}</span>
         
       </p>
